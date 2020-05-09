c490ca805df5a4d2140a92317ae9fed72daa89f0
hamza979 <hamzahasan362@gmail.com>
2020-05-09 22:22:25 +0100

    ROOMS

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, join_room, leave_room,emit
 app = Flask(__name__)
 app.config['SECRET_KEY']='mysecret'
 socketio=SocketIO(app)
@@ -9,6 +9,46 @@ socketio.init_app(app, cors_allowed_origins="*")
 def handleMessage(msg):
 	print ('Message: '+msg)
 	send (msg,broadcast=True)
+
+@socketio.on('jumba')
+def handleJumba(data):
+	print('hi')
+	emit('WE ARE IN ROOMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM: '+str(data['channel']),room=data['channel'])
+
+@socketio.on('join')
+def on_join(data):
+    room = data['channel']
+    join_room(room)
+    print('user' + ' has entered the room.'+str(room))
+  
+
+
+@socketio.on('play')
+def play(data):
+    room = data['channel']
+    time=data['time']
+    print(str(room)+str(time)+'PLAY REQUEST RECIEVED')
+    emit('play',{'time':time},room=room,broadcast=True)
+@socketio.on('pause')
+def pause(data):
+    room = data['channel']
+    time=data['time']
+    print(str(room)+str(time)+'PAUSE REQUEST RECIEVED')
+    emit('pause',{'time':time},room=room,broadcast=True)
+
+
+@socketio.on('brain')
+def on_join(data):
+    room = data['channel']
+    join_room(room)
+    print('user' + ' has entered the room.'+str(room))
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
 
 @app.route('/<int:roomID>')
 def index(roomID):
