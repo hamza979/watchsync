39afdf162c42a2511c33fb3fc2bb2d76a2e8a782
hamza979 <hamzahasan362@gmail.com>
2020-05-11 02:35:39 +0100

    testing s3buckets

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, send, join_room, leave_room,emit
+import os, json, boto3
 app = Flask(__name__)
 app.config['SECRET_KEY']='mysecret'
 socketio=SocketIO(app)
@@ -59,11 +60,48 @@ def roomN(roomID):
 @app.route('/')
 def index():
 	return render_template('index.html')
-@app.route('/create')
-def create():
-	return render_template('create.html')
+@app.route("/account/")
+def account():
+    return render_template('account.html')
+
+
 @app.route('/join')
 def join():
 	return render_template('join.html')
 if __name__ == '__main__':
 	socketio.run(app)
+
+@app.route('/sign_s3/')
+def sign_s3():
+  S3_BUCKET = os.environ.get('S3_BUCKET')
+  print('Hello')
+  file_name = request.args.get('file_name')
+  file_type = request.args.get('file_type')
+
+  s3 = boto3.client('s3')
+
+  presigned_post = s3.generate_presigned_post(
+    Bucket = S3_BUCKET,
+    Key = file_name,
+    Fields = {"acl": "public-read", "Content-Type": file_type},
+    Conditions = [
+      {"acl": "public-read"},
+      {"Content-Type": file_type}
+    ],
+    ExpiresIn = 3600
+  )
+
+  return json.dumps({
+    'data': presigned_post,
+    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
+  })
+
+@app.route("/submit_form/", methods = ["POST"])
+def submit_form():
+  username = request.form["username"]
+  full_name = request.form["full-name"]
+  avatar_url = request.form["avatar-url"]
+
+  update_account(username, full_name, avatar_url)
+
+  return redirect(url_for('profile'))
